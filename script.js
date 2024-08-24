c7a8b72e5d28aebe4370db232687153bd7613e5c
RUPAM GUMBER <77192985+rupam870@users.noreply.github.com>
2024-08-24 15:21:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function checkPassword() {
+    const password = document.getElementById("password").value;
+    const correctPassword = "123456";
+    const errorMessage = document.getElementById("error-message");
+
+    if (password === correctPassword) {
+        window.location.href = "https://rupamgumber.framer.website/xalts";
+    } else {
+        errorMessage.textContent = "Incorrect password.";
+    }
+}
+
+function goBack() {
+    window.location.href = "https://rupamgumber.framer.website/";
+}
+
+const togglePassword = document.getElementById("togglePassword");
+const passwordField = document.getElementById("password");
+const eyeIcon = document.getElementById("eyeIcon");
+
+togglePassword.addEventListener("click", function () {
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+
+    // Change the eye icon depending on the password visibility
+    if (type === "password") {
+        eyeIcon.src = "eye-slash-icon.svg"; // change to eye icon
+    } else {
+        eyeIcon.src = "eye-icon.svg"; // change to eye slash icon
+    }
+});
